0b731948bcb928f20eea32cba441fb1a8a3e6b67
Andrey Poplavskii <andris838@gmail.com>
2021-11-30 21:43:40 +0300

    comment added

--- Stack/Stack/OtusDictionary.cs
+++ Stack/Stack/OtusDictionary.cs
@@ -13,7 +13,7 @@ namespace Stack
         public OtusDictionary(int size)
         {
             arrayvalue = new Dictionary<int, string>(size);
-            
+            //123
         }
 
        public void Add(int key, string value)
