c4681ea75381b98937a29c58295b4836b356463c
Andrey Poplavskii <andris838@gmail.com>
2021-11-30 21:30:15 +0300

    init commit

---
+++ Stack/Stack/OtusDictionary.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class OtusDictionary
+    {
+        Dictionary<int, string> arrayvalue;
+
+        public OtusDictionary(int size)
+        {
+            arrayvalue = new Dictionary<int, string>(size);
+            
+        }
+
+       public void Add(int key, string value)
+        {
+            try
+            {
+                arrayvalue.Add(key, value);
+            }
+            catch(ArgumentException)
+            {
+                throw new ArgumentException($"Ошибка добавления! Элемент с ключом - {key} уже существует");
+            }
+                      
+        }
+
+        public string Get(int key)
+        {
+            try
+            {
+                return arrayvalue[key];
+            }
+            catch(NullReferenceException)
+            {
+                throw new NullReferenceException($"Элемента с индексом {key} не существует!");
+            }
+        }
+
+        public void Resize(int sizenew)
+        {
+            Dictionary<int, string> arrayvalueNew = new Dictionary<int, string>(sizenew);
+
+            for (int i = 0; i < arrayvalue.Count; i++)
+            {
+
+                arrayvalueNew[i] = arrayvalue[i];
+
+            }
+
+        }
+
+
+    }
+}
---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                OtusDictionary otusDictionary = new OtusDictionary(32);
+
+                otusDictionary.Add(0, "111");
+                otusDictionary.Add(1, "222");
+                otusDictionary.Add(2, null);
+
+                Console.WriteLine($"{otusDictionary.Get(3)}");
+
+                otusDictionary.Resize(40);
+
+               
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);            
+            }
+
+
+        }
+    }
+}
